9e121742fbb8d259115f9536ae115077c09a00ce
Diego Fornalha <37958057+diegofornalha@users.noreply.github.com>
2022-08-08 12:23:20 +0000

    replit

--- main.sh
+++ main.sh
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 ssh 'diegofornalh-sshpublico-oouciukb6ld#1vXRxzKM869AFPqRXhMGEql3jbeijtyw@diegofornalh-sshpublico-oouciukb6ld.ssh.ws-us59.gitpod.io'
+=======
+ssh 'diegofornalh-sshpublico-oouciukb6ld#1vXRxzKM869AFPqRXhMGEql3jbeijtyw@diegofornalh-sshpublico-oouciukb6ld.ssh.ws-us59.gitpod.io'
+>>>>>>> origin/main
