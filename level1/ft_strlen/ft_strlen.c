baca9de0515188c650c7d54e6a62eec17c872880
Diego Fornalha <diegofornalha@gmail.com>
2022-08-08 12:06:59 +0000

    ajuste swap

---
+++ level1/ft_strlen/ft_strlen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+    int index;
+    index = 0;
+
+    while (str[index] != '\0')
+        index ++;
+        return (index);
+}
---
+++ level1/ft_strlen/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int ft_strlen(char *str);
+
+int main (void){
+    char *str = "Diego Alcantara";
+    printf("%d\n", ft_strlen(str));
+}
